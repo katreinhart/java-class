07fc3d11393a35e00c17f2d7264726a114d3fdc8
Kat Reinhart <kat.reinhart@gmail.com>
2017-08-08 15:24:43 -0700

    Begin locations project

---
+++ InputOutput/src/com/katreinhart/Locations.java
@@ -0,0 +1,12 @@
+package com.katreinhart;
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * Created by kat on 8/8/17.
+ */
+public class Locations implements Map<Integer, Location> {
+    private static Map<Integer, Location> locations = new HashMap<Integer, Location>();
+
+
+}
